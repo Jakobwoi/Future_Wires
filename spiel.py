10e397e150ece1898c0c7d4afe4086bed06c80b5
Jakob <jakob.woittennek@yahoo.com>
2022-02-08 18:43:09 +0100

    Added Gravity, key Controling and camera

--- spiel.py
+++ spiel.py
@@ -1,57 +1,151 @@
 """
-Example of Pymunk Physics Engine Platformer
+Platformer Game
 """
 import arcade
 
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Future Wires"
 
-# Size of screen to show, in pixels
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
 
-class GameWindow(arcade.Window):
-    """ Main Window """
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
 
-    def __init__(self, width, height, title):
-        """ Create the variables """
-        Kaefer = 0
-        Dead_poeple_all = 0
-        Dead_poeple_experts = 0
-        Dead_poeple_army = 0
-        
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
 
-        
 
-        # Init the parent class
-        super().__init__(width, height, title)
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True)
+
+        # Our Scene Object
+        self.scene = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        arcade.set_background_color(arcade.csscolor.SADDLE_BROWN)
 
     def setup(self):
-        """ Set up everything with the game """
-        pass
+        """Set up the game here. Call this function to restart the game."""
+
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 15000, 64):
+            wall = arcade.Sprite(":resources:images/tiles/bridgeB.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
+        )
+
+        # Setup the Camera
+        self.camera = arcade.Camera(self.width, self.height)
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-        
-        pass
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-        pass
+        """Called when the user releases a key."""
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
-        pass
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+        # Position the camera
+        self.center_camera_to_player()
+
 
     def on_draw(self):
-        """ Draw everything """
-        arcade.start_render()
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        self.clear()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate our Camera
+        self.camera.use()
+
 
 
 def main():
-    """ Main function """
-    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    """Main function"""
+    window = MyGame()
     window.setup()
     arcade.run()
 
--- spiel_sprites.py
+++ spiel_sprites.py
@@ -1,16 +1,14 @@
 """
-Platformer Game
+Example showing how handle screen resizing.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.resizable_window
 """
 import arcade
 
-# Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
-
-# Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 1
-TILE_SCALING = 0.5
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+SCREEN_TITLE = "Resizing Window Example"
 
 
 class MyGame(arcade.Window):
@@ -18,69 +16,30 @@ class MyGame(arcade.Window):
     Main application class.
     """
 
-    def __init__(self):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, resizable=True)
 
-        # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.WHITE)
 
-        # These are 'lists' that keep track of our sprites. Each sprite should
-        # go into a list.
-        self.wall_list = None
-        self.player_list = None
+    def on_resize(self, width, height):
+        """ This method is automatically called when the window is resized. """
 
-        # Separate variable that holds the player sprite
-        self.player_sprite = None
+        # Call the parent. Failing to do this will mess up the coordinates,
+        # and default to 0,0 at the center and the edges being -1 to 1.
+        super().on_resize(width, height)
 
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
-
-    def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        # Create the Sprite lists
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-
-        # Set up the player, specifically placing it at these coordinates.
-        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
-        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
-        self.player_list.append(self.player_sprite)
-
-        # Create the ground
-        # This shows using a loop to place multiple sprites horizontally
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.wall_list.append(wall)
-
-        # Put some crates on the ground
-        # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
-
-        for coordinate in coordinate_list:
-            # Add a crate on the ground
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
-            )
-            wall.position = coordinate
-            self.wall_list.append(wall)
+        print(f"Window resized to: {width}, {height}")
 
     def on_draw(self):
-        """Render the screen."""
+        """ Render the screen. """
 
-        # Clear the screen to the background color
         self.clear()
 
-        # Draw our sprites
-        self.wall_list.draw()
-        self.player_list.draw()
+
 
 
 def main():
-    """Main function"""
-    window = MyGame()
-    window.setup()
+    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
 
 
